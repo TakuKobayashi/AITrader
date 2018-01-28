2397d76339dca44c8d1aa75a25763d018dec0427
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-01-29 05:20:03 +0900

    リアルタイムにデータを取得し、データを突っ込んでいく処理は一応できた

--- app/models/concerns/ai_brain.rb
+++ app/models/concerns/ai_brain.rb
@@ -1,9 +1,9 @@
 module AiBrain
   def self.trace!
-    currency_pair_names = Mst::CurrencyPair.where(is_token: false).pluck(:pair_name).select{|name| name.present? }
+    currency_name_pairs = Mst::CurrencyPair.where(is_token: false).select{|pair| pair.pair_name.present? }.index_by(&:pair_name)
     EM.run do
-      currency_pair_names.each do |pair_name|
-        ws = Faye::WebSocket::Client.new('wss://ws.zaif.jp/stream?currency_pair=' + pair_name)
+      currency_name_pairs.each do |pair_name, currency_pair|
+        ws = Faye::WebSocket::Client.new('wss://ws.zaif.jp/stream?currency_pair=' + currency_pair.pair_name)
 
         ws.on :open do |event|
           p "open"
@@ -11,7 +11,7 @@ module AiBrain
 
         ws.on :message do |event|
           trade_json = JSON.parse(event.data)
-          p trade_json["currency_pair"]
+          self.input!(currency_name_pairs[trade_json["currency_pair"]], trade_json)
         end
 
         ws.on :close do |event|
@@ -20,5 +20,29 @@ module AiBrain
         end
       end
     end
+  end
+
+  def self.input!(currency_pair, json)
+    log_trades = json["trades"].map do |trade_hash|
+      Log::Trade.new(
+        mst_exchange_id: currency_pair.mst_exchange_id,
+        mst_currency_pair_id: currency_pair.id,
+        tid: trade_hash["tid"],
+        trade_method: trade_hash["trade_type"],
+        price: trade_hash["price"],
+        amount: trade_hash["amount"],
+        traded_time: Time.at(trade_hash["date"])
+      )
+    end
+    Log::Trade.import!(log_trades, on_duplicate_key_update: [:trade_method, :price, :amount, :traded_time])
+    return log_trades
+  end
+
+  def self.imagine!
+
+  end
+
+  def self.judge!
+
   end
 end
