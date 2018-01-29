d1e2cabf03e53b767b392e74248d45ed8bbea5de
TakuKobayshi <>
2018-01-29 14:45:04 +0900

    Walletを二つに分ける

---
+++ app/models/investment_wallet.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: wallets
+#
+#  id              :integer          not null, primary key
+#  mst_exchange_id :integer          not null
+#  mst_currency_id :integer          not null
+#  amount          :float(24)        default(0.0), not null
+#  type            :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+#ガンガン動かすお金
+class InvestmentWallet < Wallet
+end
