d2129940456e77b8d89d0440e8f3aa48deb6f39c
TakuKobayshi <>
2018-01-29 14:36:23 +0900

    botにつぶやいた情報とかを記録しておくように

--- app/models/twitter_bot.rb
+++ app/models/twitter_bot.rb
@@ -18,4 +18,29 @@
 #
 
 class TwitterBot < ApplicationRecord
+  belongs_to :from, polymorphic: true, required: false
+
+  def self.tweet!(text:, from: nil, options: {})
+    twitter_client = get_twitter_client
+    tweet_result = twitter_client.update(text, options)
+    twitter_bot = TwitterBot.create!(tweet: tweet_result.text, tweet_id: tweet_result.id, tweet_time: tweet_result.created_at, from: from)
+    return twitter_bot
+  end
+
+  def reject_tweet!
+    twitter_client = get_twitter_client
+    result = twitter_client.destroy_status(self.tweet_id)
+    destroy!
+  end
+
+  def self.get_twitter_client
+    apiconfig = YAML.load(File.open(Rails.root.to_s + "/config/apiconfig.yml"))
+    twitter_client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = apiconfig["twitter"]["consumer_key"]
+      config.consumer_secret     = apiconfig["twitter"]["consumer_secret"]
+      config.access_token        = apiconfig["twitter"]["bot"]["access_token_key"]
+      config.access_token_secret = apiconfig["twitter"]["bot"]["access_token_secret"]
+    end
+    return twitter_client
+  end
 end
